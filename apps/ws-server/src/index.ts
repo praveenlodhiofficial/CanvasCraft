96e114fd36619a5a79b16731e1e8019605364e5f
praveenlodhi99 <praveenlodhi.official@gmail.com>
2025-03-27 23:25:33 +0530

    feat(apps/ws-server): implement WebSocket server with JWT authentication
    - Added WebSocket server setup using 'ws' library
    - Implemented JWT authentication for user connections
    - Created user management system with User interface and array storage
    - Added connection handling with token verification

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,12 +1,65 @@
-import { WebSocketServer } from "ws";
-import { config } from "@repo/configuration/config";
+import jwt from 'jsonwebtoken'
+import WebSocket, { WebSocketServer } from "ws";
+import { config } from '@repo/configuration/config'
+// import { prismaClient } from '@repo/'
 
-const wss = new WebSocketServer({ port: config.WS_SERVER_PORT});   
+interface User {
+    userId: string
+    rooms: number[],
+    ws: WebSocket
+}
 
-wss.on("connection", (ws) => {
+const wss = new WebSocketServer({ port: config.WS_SERVER_PORT })
+const users: User[] = [];
 
-    ws.on("message", (message) => {
-        ws.send(message);
-        console.log(`Received message: ${message}`);
+function cheakUser(token: string): string | null {
+    const decoded = jwt.verify(token, config.JWT_SECRET)
+
+    if (typeof decoded === 'string' || !decoded || !decoded.userId) {
+        return null
+    }
+
+    return decoded.userId;
+}
+
+wss.on('connection', (ws, request) => {
+    const url = request.url;
+
+    if (!url) {
+        ws.close(1003, 'URL not provided')
+        return
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1])
+    const token = queryParams.get('token') || ' ';
+    const userId = cheakUser(token)
+
+    if (userId === null) {
+        ws.close(1003, 'User not Authernticated')
+        return
+    }
+
+    users.push({
+        userId,
+        ws,
+        rooms: []
     })
-});
+
+    console.log(`User Connected: ${userId}`);
+
+    wss.on('message', async(data) => {
+        let parsedData;
+
+        if (typeof data === 'string') {
+            parsedData = JSON.parse(data.toString());
+        } else {
+            parsedData = JSON.parse(data)
+        }
+
+        if (parsedData.type === 'join_room') {
+            const roomId = Number(parsedData.roomId)
+            const user = users.find((x) => x.ws === ws)
+        }
+    })
+
+})
